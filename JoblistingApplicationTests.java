af74b591c3ea020c0455fce13cd12af759665b77
Harshada Jadhav <144210898+Harshada305@users.noreply.github.com>
2024-04-03 14:20:38 +0530

    Create JoblistingApplicationTests.java

---
+++ JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.own.dreamjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
